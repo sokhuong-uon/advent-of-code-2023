9fb996dfc8d26fb968e1c9d5ea35203b54a0bcc5
Sokhuong Uon <sokhuong.usk@gmail.com>
2023-12-01 16:58:21 +0700

    d1p1

--- src/bin/sol.rs
+++ src/bin/sol.rs
@@ -5,17 +5,35 @@ fn main() {
     dbg!(output);
 }
 
-fn p1(input: &str) -> String {
-    "hi".to_string()
-}
+fn p1(input: &str) -> i32 {
+    let mut pair = (' ', ' ');
+    let mut sum = 0;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    for line in input.lines() {
+        let chars_vec = line.chars().collect::<Vec<char>>();
+        let mut left = 0;
+        let mut right = chars_vec.len() - 1;
 
-    #[test]
-    fn it_works() {
-        let result = p1("");
-        assert_eq!(result, "hi");
+        while left <= right {
+            if pair.0 != ' ' && pair.1 != ' ' {
+                break;
+            }
+
+            if chars_vec[left].is_digit(10) {
+                pair.0 = chars_vec[left];
+            } else {
+                left += 1;
+            }
+            if chars_vec[right].is_digit(10) {
+                pair.1 = chars_vec[right];
+            } else {
+                right -= 1;
+            }
+        }
+        let s = format!("{}{}", pair.0, pair.1);
+        sum += s.parse::<i32>().unwrap();
+
+        pair = (' ', ' ');
     }
+    sum
 }
