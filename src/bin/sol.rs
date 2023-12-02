e70068938d6d2eb8c3395c22fe5c0fdba1adf5d8
Sokhuong Uon <sokhuong.usk@gmail.com>
2023-12-02 13:44:20 +0700

    d2p1

--- src/bin/sol.rs
+++ src/bin/sol.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Read;
 
 fn main() {
     let mut file = File::open("./src/bin/in.txt").unwrap();
@@ -8,128 +8,54 @@ fn main() {
     println!("{}", p1(&contents));
 }
 
-fn map_number(str: &str) -> &str {
-    match str {
-        "one" => "1",
-        "two" => "2",
-        "three" => "3",
-        "four" => "4",
-        "five" => "5",
-        "six" => "6",
-        "seven" => "7",
-        "eight" => "8",
-        "nine" => "9",
-        _ => str,
-    }
+fn extract_game_data(game: &str) -> (u32, Vec<(u32, u32, u32)>) {
+    let game_data = game.split(":").collect::<Vec<&str>>();
+
+    let game_id = game_data[0].split(" ").collect::<Vec<&str>>()[1]
+        .parse::<u32>()
+        .unwrap();
+
+    let sets = game_data[1]
+        .split(";")
+        .map(process_set)
+        .collect::<Vec<(u32, u32, u32)>>();
+
+    (game_id, sets)
 }
+fn process_set(set: &str) -> (u32, u32, u32) {
+    let mut rgb = (0, 0, 0);
 
-fn write_to_file(
-    file: &mut File,
-    line: &str,
-    digit_pair: (&str, &str),
-    numeric: &str,
-    file_name: &str,
-) {
-    writeln!(file, "").unwrap();
-
-    writeln!(file, "{line}").unwrap();
-    writeln!(file, "{} {}", digit_pair.0, digit_pair.1).unwrap();
-    writeln!(file, "{numeric}").unwrap();
-}
-
-fn separate_input(input: &str) {
-    let ranges = [
-        (1, 0, 50),
-        (2, 50, 100),
-        (3, 100, 150),
-        (4, 150, 200),
-        (5, 200, 250),
-        (6, 250, 300),
-        (7, 300, 350),
-        (8, 350, 400),
-        (9, 400, 450),
-        (10, 450, 500),
-        (11, 500, 550),
-        (12, 550, 600),
-        (13, 600, 650),
-        (14, 650, 700),
-        (15, 700, 750),
-        (16, 750, 800),
-        (17, 800, 850),
-        (18, 850, 900),
-        (19, 900, 950),
-        (20, 950, 1000),
-    ];
-
-    for (i, (file_name, start, end)) in ranges.iter().enumerate() {
-        let mut file = File::options()
-            .append(true)
-            .open(format!("./src/bin/{file_name}.txt"))
-            .unwrap();
-
-        for (index, line) in input.lines().enumerate() {
-            if index >= *start && index < *end {
-                writeln!(file, "{}", line).unwrap();
-            }
+    set.trim().split(",").for_each(|cube| {
+        let cube_data = cube.trim().split(" ").collect::<Vec<&str>>();
+        match cube_data[1] {
+            "red" => rgb.0 = cube_data[0].parse::<u32>().unwrap(),
+            "green" => rgb.1 = cube_data[0].parse::<u32>().unwrap(),
+            "blue" => rgb.2 = cube_data[0].parse::<u32>().unwrap(),
+            _ => (),
         }
-    }
+    });
+
+    rgb
 }
 
-fn create_file(name: &str) -> File {
-    let mut file = File::create(format!("./src/bin/{name}.txt")).unwrap();
-    file
-}
-
-fn p1(input: &str) -> i64 {
-    // let mut file = File::options()
-    //     .append(true)
-    //     .open("./src/bin/out.txt")
-    //     .unwrap();
-
-    // writeln!(file, "").unwrap();
-    // writeln!(file, "{file_name} ==========").unwrap();
-
+fn p1(input: &str) -> u32 {
     let mut sum = 0;
-
-    let matches = [
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
-        "seven", "eight", "nine",
-    ];
+    let rgb = (12, 13, 14);
 
     for line in input.lines() {
-        let mut digit_pair = ("", "");
+        let game_data = extract_game_data(line);
 
-        let mut left_most_index = line.len();
-        let mut right_most_index: isize = -1;
+        let mut possible = true;
 
-        for (index, number) in matches.iter().enumerate() {
-            let left_option = line.find(number);
-            if left_option.is_some() {
-                let i = left_option.unwrap();
-                if i < left_most_index {
-                    left_most_index = i;
-                    digit_pair.0 = matches[index];
-                }
-            }
-
-            let right_option = line.rfind(number);
-            println!("right most: {:?}", right_most_index);
-            if right_option.is_some() {
-                let i = right_option.unwrap();
-                println!("{:?}", right_option);
-                println!("{:?}", i);
-                if i as isize > right_most_index {
-                    right_most_index = i as isize;
-                    digit_pair.1 = matches[index];
-                }
+        for set in game_data.1 {
+            if set.0 > rgb.0 || set.1 > rgb.1 || set.2 > rgb.2 {
+                possible = false;
             }
         }
 
-        let numeric = format!("{}{}", map_number(digit_pair.0), map_number(digit_pair.1));
-
-        sum += numeric.parse::<i64>().unwrap();
-
-        // write_to_file(&mut file, line, digit_pair, &numeric, file_name);
+        if possible {
+            sum += game_data.0;
+        }
     }
 
     sum
