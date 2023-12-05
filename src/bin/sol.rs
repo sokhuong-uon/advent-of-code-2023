0ae9679440efe8d2f183dbd3e47b7501b73d5391
Sokhuong Uon <sokhuong.usk@gmail.com>
2023-12-05 20:18:56 +0700

    d5p1: nice

--- src/bin/sol.rs
+++ src/bin/sol.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
@@ -9,83 +8,95 @@ fn main() {
     println!("total: {}", solution(&contents));
 }
 
-fn collect_seed(line: &str) -> Vec<u32> {
+fn collect_seed(line: &str) -> Vec<u64> {
     line.split(":").collect::<Vec<&str>>()[1]
         .trim()
         .split(" ")
-        .filter_map(|numeric_string| numeric_string.parse::<u32>().ok())
-        .collect::<Vec<u32>>()
+        .filter_map(|numeric_string| numeric_string.parse::<u64>().ok())
+        .collect::<Vec<u64>>()
 }
 
-fn populate_map(map: &mut Map, line: &str) {
-    let map_numbers = line
+fn populate_map(map: &mut Vec<MapItem>, line: &str) {
+    let map_item = line
         .split(" ")
-        .map(|numeric_string| numeric_string.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .map(|numeric_string| numeric_string.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
 
-    for i in 0..map_numbers[2] {
-        map.insert(map_numbers[1] + i, map_numbers[0] + i);
+    map.push(MapItem::new(map_item[0], map_item[1], map_item[2]));
+}
+fn find_location_from_seed(maps: &Maps, seed: &u64) -> u64 {
+    let mut location = *seed;
+    location = find_destination(&maps.seed_to_soil, &location);
+    location = find_destination(&maps.soil_to_fertilizer, &location);
+    location = find_destination(&maps.fertilizer_to_water, &location);
+    location = find_destination(&maps.water_to_light, &location);
+    location = find_destination(&maps.light_to_temperature, &location);
+    location = find_destination(&maps.temperature_to_humidity, &location);
+    location = find_destination(&maps.humidity_to_location, &location);
+    location
+}
+
+fn find_destination(map: &Vec<MapItem>, source: &u64) -> u64 {
+    let mut destination = *source;
+    for map_item in map {
+        if let Some(d) = map_item.find_destination(source) {
+            destination = d;
+            break;
+        }
+    }
+    destination
+}
+
+struct MapItem {
+    destination_start: u64,
+    source_start: u64,
+    range: u64,
+}
+impl MapItem {
+    fn new(destination_start: u64, source_start: u64, range: u64) -> MapItem {
+        MapItem {
+            destination_start,
+            source_start,
+            range,
+        }
+    }
+
+    fn find_destination(&self, source: &u64) -> Option<u64> {
+        if self.source_start <= *source && *source <= self.source_start + self.range - 1 {
+            let extra = *source - self.source_start;
+            Some(self.destination_start + extra)
+        } else {
+            None
+        }
     }
 }
 
 struct Maps {
-    seed_to_soil: Map,
-    soil_to_fertilizer: Map,
-    fertilizer_to_water: Map,
-    water_to_light: Map,
-    light_to_temperature: Map,
-    temperature_to_humidity: Map,
-    humidity_to_location: Map,
+    seed_to_soil: Vec<MapItem>,
+    soil_to_fertilizer: Vec<MapItem>,
+    fertilizer_to_water: Vec<MapItem>,
+    water_to_light: Vec<MapItem>,
+    light_to_temperature: Vec<MapItem>,
+    temperature_to_humidity: Vec<MapItem>,
+    humidity_to_location: Vec<MapItem>,
 }
 
-fn find_location_for_seed(maps: &Maps, seed: &u32) -> u32 {
-    let soil = maps.seed_to_soil.get(&seed).unwrap_or_else(|| &seed);
-
-    let fertilizer = maps.soil_to_fertilizer.get(soil).unwrap_or_else(|| soil);
-
-    let water = maps
-        .fertilizer_to_water
-        .get(fertilizer)
-        .unwrap_or_else(|| fertilizer);
-
-    let light = maps.water_to_light.get(water).unwrap_or_else(|| water);
-
-    let temperature = maps
-        .light_to_temperature
-        .get(light)
-        .unwrap_or_else(|| light);
-
-    let humidity = maps
-        .temperature_to_humidity
-        .get(temperature)
-        .unwrap_or_else(|| temperature);
-
-    let location = maps
-        .humidity_to_location
-        .get(humidity)
-        .unwrap_or_else(|| humidity);
-
-    *location
-}
-
-type Map = HashMap<u32, u32>;
-
-fn solution(input: &str) -> u32 {
-    let mut seeds: Vec<u32> = vec![];
+fn solution(input: &str) -> u64 {
+    let mut seeds: Vec<u64> = vec![];
 
     let mut maps = Maps {
-        seed_to_soil: HashMap::new(),
-        soil_to_fertilizer: HashMap::new(),
-        fertilizer_to_water: HashMap::new(),
-        water_to_light: HashMap::new(),
-        light_to_temperature: HashMap::new(),
-        temperature_to_humidity: HashMap::new(),
-        humidity_to_location: HashMap::new(),
+        seed_to_soil: vec![],
+        soil_to_fertilizer: vec![],
+        fertilizer_to_water: vec![],
+        water_to_light: vec![],
+        light_to_temperature: vec![],
+        temperature_to_humidity: vec![],
+        humidity_to_location: vec![],
     };
 
-    let mut should_populate_map_on_next_iteration = false;
-
     let mut current_map = &mut maps.seed_to_soil;
+
+    let mut should_populate_map_on_next_iteration = false;
 
     for line in input.lines() {
         println!("line: {:?}", line);
@@ -100,38 +111,27 @@ fn solution(input: &str) -> u32 {
         }
 
         if should_populate_map_on_next_iteration {
-            populate_map(&mut current_map, line);
+            populate_map(current_map, line);
             continue;
         }
 
-        if line == "seed-to-soil map:" {
-            current_map = &mut maps.seed_to_soil;
-            should_populate_map_on_next_iteration = true;
-        } else if line == "soil-to-fertilizer map:" {
-            current_map = &mut maps.soil_to_fertilizer;
-            should_populate_map_on_next_iteration = true;
-        } else if line == "fertilizer-to-water map:" {
-            current_map = &mut maps.fertilizer_to_water;
-            should_populate_map_on_next_iteration = true;
-        } else if line == "water-to-light map:" {
-            current_map = &mut maps.water_to_light;
-            should_populate_map_on_next_iteration = true;
-        } else if line == "light-to-temperature map:" {
-            current_map = &mut maps.light_to_temperature;
-            should_populate_map_on_next_iteration = true;
-        } else if line == "temperature-to-humidity map:" {
-            current_map = &mut maps.temperature_to_humidity;
-            should_populate_map_on_next_iteration = true;
-        } else if line == "humidity-to-location map:" {
-            current_map = &mut maps.humidity_to_location;
-            should_populate_map_on_next_iteration = true;
-        }
+        current_map = match line {
+            "seed-to-soil map:" => &mut maps.seed_to_soil,
+            "soil-to-fertilizer map:" => &mut maps.soil_to_fertilizer,
+            "fertilizer-to-water map:" => &mut maps.fertilizer_to_water,
+            "water-to-light map:" => &mut maps.water_to_light,
+            "light-to-temperature map:" => &mut maps.light_to_temperature,
+            "temperature-to-humidity map:" => &mut maps.temperature_to_humidity,
+            "humidity-to-location map:" => &mut maps.humidity_to_location,
+            _ => panic!("unknown map"),
+        };
+        should_populate_map_on_next_iteration = true;
     }
 
     let might = seeds
         .iter()
-        .map(|seed| find_location_for_seed(&maps, seed))
-        .fold(u32::MAX, |acc, v| if v < acc { v } else { acc });
+        .map(|seed| find_location_from_seed(&maps, seed))
+        .fold(u64::MAX, |acc, v| if v < acc { v } else { acc });
 
     println!("might: {:?}", might);
 
@@ -154,84 +154,81 @@ mod tests {
 
     #[test]
     fn it_can_populate_map() {
-        let mut map = super::Map::new();
+        let mut map = vec![];
+        let line = "1 2 3";
+
+        super::populate_map(&mut map, line);
+        assert_eq!(map[0].destination_start, 1);
+        assert_eq!(map[0].source_start, 2);
+        assert_eq!(map[0].range, 3);
+
         let line = "50 98 2";
-
         super::populate_map(&mut map, line);
-
-        assert_eq!(map.len(), 2);
-        assert_eq!(map.get(&98), Some(&50));
-        assert_eq!(map.get(&99), Some(&51));
-
-        //
-        let mut map = super::Map::new();
-        let line = "52 50 48";
-
-        super::populate_map(&mut map, line);
-
-        assert_eq!(map.len(), 48);
-        assert_eq!(map.get(&50), Some(&52));
-        assert_eq!(map.get(&51), Some(&53));
-        for i in 50..98 {
-            assert_eq!(map.get(&i), Some(&(i + 2)));
-        }
-
-        //
-        let mut map = super::Map::new();
-        let line = "50 98 100";
-
-        super::populate_map(&mut map, line);
-
-        assert_eq!(map.len(), 100);
-        assert_eq!(map.get(&98), Some(&50));
-        assert_eq!(map.get(&99), Some(&51));
-        assert_eq!(map.get(&100), Some(&52));
-        for i in 98..198 {
-            assert_eq!(map.get(&i), Some(&(i - 48)));
-        }
+        assert_eq!(map[1].destination_start, 50);
+        assert_eq!(map[1].source_start, 98);
+        assert_eq!(map[1].range, 2);
     }
 
     #[test]
-    fn it_can_find_location_for_seed() {
+    fn it_can_find_destination_per_map_item() {
+        let map_item = super::MapItem::new(50, 98, 2);
+
+        assert_eq!(map_item.find_destination(&97), None);
+        assert_eq!(map_item.find_destination(&98), Some(50));
+        assert_eq!(map_item.find_destination(&99), Some(51));
+        assert_eq!(map_item.find_destination(&100), None);
+
+        let map_item = super::MapItem::new(52, 50, 48);
+
+        assert_eq!(map_item.find_destination(&49), None);
+        assert_eq!(map_item.find_destination(&50), Some(52));
+        assert_eq!(map_item.find_destination(&51), Some(53));
+        assert_eq!(map_item.find_destination(&97), Some(99));
+        assert_eq!(map_item.find_destination(&99), None);
+    }
+
+    #[test]
+    fn it_can_find_destination_per_map() {
+        let mut map = vec![];
+
+        let line = "50 98 2";
+        super::populate_map(&mut map, line);
+
+        let line = "52 50 48";
+        super::populate_map(&mut map, line);
+
+        assert_eq!(super::find_destination(&map, &49), 49);
+        assert_eq!(super::find_destination(&map, &50), 52);
+        assert_eq!(super::find_destination(&map, &51), 53);
+        assert_eq!(super::find_destination(&map, &97), 99);
+    }
+    #[test]
+    fn it_can_find_location_from_seed() {
+        let seed = 10;
+
         let mut maps = super::Maps {
-            seed_to_soil: super::Map::new(),
-            soil_to_fertilizer: super::Map::new(),
-            fertilizer_to_water: super::Map::new(),
-            water_to_light: super::Map::new(),
-            light_to_temperature: super::Map::new(),
-            temperature_to_humidity: super::Map::new(),
-            humidity_to_location: super::Map::new(),
+            seed_to_soil: vec![],
+            soil_to_fertilizer: vec![],
+            fertilizer_to_water: vec![],
+            water_to_light: vec![],
+            light_to_temperature: vec![],
+            temperature_to_humidity: vec![],
+            humidity_to_location: vec![],
         };
 
-        let seed = 3;
+        maps.seed_to_soil.push(super::MapItem::new(5, 8, 3)); // 10 -> 7
+        maps.soil_to_fertilizer.push(super::MapItem::new(10, 5, 5)); // 7 -> 12
+        maps.fertilizer_to_water
+            .push(super::MapItem::new(15, 10, 5)); // 12 -> 17
+        maps.water_to_light.push(super::MapItem::new(50, 80, 100)); // 17 -> 17
+        maps.light_to_temperature
+            .push(super::MapItem::new(100, 15, 100)); // 17 -> 102
+        maps.temperature_to_humidity
+            .push(super::MapItem::new(88, 100, 5)); // 102 -> 90
+        maps.humidity_to_location
+            .push(super::MapItem::new(100, 88, 12)); // 90 -> 102
 
-        maps.seed_to_soil.insert(3, 1);
-        maps.seed_to_soil.insert(2, 5);
-
-        maps.soil_to_fertilizer.insert(1, 2);
-        maps.soil_to_fertilizer.insert(5, 100);
-
-        maps.fertilizer_to_water.insert(2, 15);
-        maps.fertilizer_to_water.insert(45, 4);
-
-        maps.water_to_light.insert(3, 4);
-        maps.water_to_light.insert(15, 8);
-
-        maps.light_to_temperature.insert(4, 5);
-        maps.light_to_temperature.insert(34, 99);
-
-        maps.temperature_to_humidity.insert(5, 6);
-        maps.temperature_to_humidity.insert(8, 25);
-
-        maps.humidity_to_location.insert(6, 7);
-        maps.humidity_to_location.insert(32, 9);
-        maps.humidity_to_location.insert(100, 69);
-
-        let location = super::find_location_for_seed(&maps, &seed);
-        assert_eq!(location, 25);
-
-        let seed = 2;
-        let location = super::find_location_for_seed(&maps, &seed);
-        assert_eq!(location, 69);
+        let location = super::find_location_from_seed(&maps, &seed);
+        assert_eq!(location, 102);
     }
 }
